7ff3876621d3b8a5ea74f046eb737e5c30019b8c
Reyona <nokoru_ryohei_00@163.com>
2017-12-05 00:07:19 +0800

    add mint-ui components

---
+++ public/javascripts/index1.mobile.js
@@ -0,0 +1,18 @@
+// import Vue from 'vue'
+// import MintUI from 'mint-ui'
+// import 'mint-ui/lib/style.css'
+// import App from './App.vue'
+
+// Vue.use(MintUI);
+
+new Vue({
+    el: '#app',
+    methods: {
+        onClick: function() {
+            this.$toast('Hello world!')
+        }
+    },
+    data: {
+        selected: '2'
+    },
+});
